09572f31f49694b409ba6460bb6655c3298668a1
sangwoo <tkddn3367@gmail.com>
2023-04-09 19:36:59 +0900

    config API gateway for a function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-module.exports.handler = async (event) => {
+module.exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: `Go Serverless v3.0! Your function executed successfully! : ${process.env.FOO}`,
-        input: event
+        input: event,
+        context
       },
       null,
       2
